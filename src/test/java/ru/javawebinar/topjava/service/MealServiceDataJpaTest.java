b129d8bc1df3af90305a5892487699e37d81429c
AlxBBB <apbukreev@gmail.com>
2017-08-13 21:23:10 +1000

    HW5  with tests+

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceDataJpaTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.service;
+
+
+import org.junit.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("datajpa")
+public class MealServiceDataJpaTest extends MealServiceTest {
+
+
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceDataJpaTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@ActiveProfiles("datajpa")
+public class UserServiceDataJpaTest extends UserServiceTest{
+
+}
